ef7ab4ef79eda2a2c114cab5c75ca589c7f9d8f7
wolfram77 <wolfram77@gmail.com>
2020-03-21 15:52:45 +0530

    src: +chunk

---
+++ src/chunk.js
@@ -0,0 +1,16 @@
+/**
+ * Breaks iterable into chunks of given size.
+ * @param {Iterable} x an iterable
+ * @param {number?} n chunk size
+ * @returns {Iterable<Array>}
+ */
+function* chunk(x, n=1) {
+  var a = [], m = 0;
+  for(var v of x) {
+    a.push(v);
+    if(++m<n) continue;
+    yield a;
+    a = []; m = 0;
+  }
+}
+module.exports = chunk;
