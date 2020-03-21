9925b980fc789dc5796de659344475d245bbc93f
wolfram77 <wolfram77@gmail.com>
2020-03-21 09:52:12 +0530

    src: +forEach

---
+++ src/forEach.js
@@ -0,0 +1,12 @@
+/**
+ * Calls a function for each value.
+ * @param {Iterable} x an iterable
+ * @param {function} fn called function (v, i, x)
+ * @param {object?} ths this argument
+ */
+function forEach(x, fn=null, ths=null) {
+  var i = -1;
+  for(var v of x)
+    fn.call(ths, v, ++i, x);
+}
+module.exports = forEach;
