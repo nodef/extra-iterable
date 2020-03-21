90a9028be3267189f81eecd1f338fb89fe0705ad
wolfram77 <wolfram77@gmail.com>
2020-03-21 16:28:44 +0530

    src: +isEqual

---
+++ src/isEqual.js
@@ -0,0 +1,13 @@
+const compare = require('./compare');
+
+/**
+ * Checks if two iterables are equal.
+ * @param {Iterable} x an iterable
+ * @param {Iterable} y an iterable
+ * @param {function?} fn compare function (a, b)
+ * @returns {boolean}
+ */
+function isEqual(x, y, fn=null) {
+  return compare(x, y, fn)===0;
+}
+module.exports = isEqual;
