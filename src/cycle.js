15d2864a6d3d906abac114893135110044d29a04
wolfram77 <wolfram77@gmail.com>
2020-03-21 15:25:40 +0530

    src: +cycle

---
+++ src/cycle.js
@@ -0,0 +1,13 @@
+/**
+ * Gives values that cycle through an iterable.
+ * @param {Iterable} x an iterable
+ * @param {number} n number of values
+ * @returns {Iterable}
+ */
+function cycle(x, n) {
+  while(true) for(var v of x) {
+    if(--n<0) break;
+    yield v;
+  }
+}
+module.exports = cycle;
