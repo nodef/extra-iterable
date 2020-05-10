d74a43a3ca075a8857e57df5f4ac73723166a942
Subhajit Sahu <wolfram77@gmail.com>
2020-05-10 07:38:14 +0530

    wiki needed edit first

--- src/length.ts
+++ src/length.ts
@@ -3,8 +3,8 @@ import size from './size';
 /**
  * Counts the number of values.
  * @param x an iterable
- * @param i start index (-ve: from right) (0)
- * @param I end index (-ve: from right) (end)
+ * @param i start index (0)
+ * @param I end index (end)
  */
 function length<T>(x: Iterable<T>, i: number=0, I: number=Number.MAX_SAFE_INTEGER): number {
   return size(x, i, I);
