35ced7dea2a43d463316449a9cd6a014130c8260
Subhajit Sahu <wolfram77@gmail.com>
2020-05-13 14:31:01 +0530

    :detective: there is now an alternate implementation for product

--- src/product.ts
+++ src/product.ts
@@ -8,7 +8,7 @@ import type {mapFn} from './_types';
  * @param fn map function (vs, i, xs)
  * @param ths this argument
  */
-function* product<T, U>(xs: Iterable<T>[], fn: mapFn<T[], U>=null, ths: object=null): IterableIterator<U> {
+function* productLoopAll<T, U>(xs: Iterable<T>[], fn: mapFn<T[], U>=null, ths: object=null): IterableIterator<U> {
   var fn = fn||id;
   var X = xs.length;
   if(X===0) return;
@@ -32,4 +32,30 @@ function* product<T, U>(xs: Iterable<T>[], fn: mapFn<T[], U>=null, ths: object=n
     }
   }
 }
-export default product;
+
+function* productLoopFirst<T, U>(xs: Iterable<T>[], fn: mapFn<T[], U>, ths: object=null): IterableIterator<U> {
+  var fn = fn||id;
+  var X = xs.length;
+  if(X===0) return;
+  var x0 = xs[0], i = -1;
+  var xs = xs.slice(1).map(many);
+  var is = xs.map(x => x[Symbol.iterator]());
+  var os = is.map(i => i.next());
+  if(os.some(o => o.done)) return;
+  for(var v0 of x0) {
+    for(;;) {
+      var vs = [v0];
+      for(var o of os) vs.push(o.value);
+      yield fn.call(ths, vs, ++i, xs);
+      for(var r=X-2; r>=0; r--) {
+        os[r] = is[r].next();
+        if(!os[r].done) break;
+        is[r] = xs[r][Symbol.iterator]();
+        os[r] = is[r].next();
+      }
+      if(r<0) break;
+    }
+  }
+}
+
+export default productLoopFirst;
