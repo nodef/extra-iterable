a23616d0e4c088a21ae2bf1cc994c90f1db6f771
Subhajit Sahu <wolfram77@gmail.com>
2020-05-13 18:12:47 +0530

    :detective: +indexOf(), lastIndexOf()

---
+++ src/lastIndexOf.ts
@@ -0,0 +1,13 @@
+import searchRight from './searchRight';
+import slice from './slice';
+
+/**
+ * Finds last index of a value.
+ * @param x an iterable
+ * @param v search value
+ * @param i start index
+ */
+function lastIndexOf<T>(x: Iterable<T>, v: T, i: number=Number.MAX_SAFE_INTEGER-1) {
+  return searchRight(slice(x, 0, i+1), v);
+}
+export default lastIndexOf;
