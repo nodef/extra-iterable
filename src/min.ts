db2c3b415f3a79a2800254d823e0df6d55474048
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 20:33:35 +0530

    :watermelon: cool fruit +max, min, range

--- src/min.ts
+++ src/min.ts
@@ -1,38 +1,13 @@
-import id from './_id';
-import cmp from './_cmp';
+import range from './range';
 import type {mapFn, compareFn} from './_types';
 
-function minCompare<T>(x: Iterable<T>, fn: compareFn<T>=null): T {
-  var fn = fn||cmp;
-  var m: T, i = -1;
-  for(var v of x)
-    if(++i===0 || fn(v, m)<0) m = v;
-  return m;
-}
-
-function minMap<T, U=T>(x: Iterable<T>, fn: mapFn<T, T|U>=null): T {
-  var fn = fn||id;
-  var mk: T|U, mv: T, i = -1;
-  for(var v of x) {
-    var k = fn(v, ++i, x);
-    if(i===0 || k<mk) { mk = k; mv = v; }
-  }
-  return mv;
-}
-
 /**
- * Finds smallest value.
+ * Finds smallest entry.
  * @param x an iterable
  * @param fc compare function (a, b)
  * @param fm map function (v, i, x)
  */
-function min<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): T {
-  var fc = fc||cmp, fm = fm||id;
-  var mk: T|U, mv: T, i = -1;
-  for(var v of x) {
-    var k = fm(v, ++i, x);
-    if(i===0 || fc(k, mk)<0) { mk = k; mv = v; }
-  }
-  return mv;
+function min<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): [number, T] {
+  return range(x, fc, fm)[0];
 }
 export default min;
--- src/range.ts
+++ src/range.ts
@@ -2,45 +2,23 @@ import id from './_id';
 import cmp from './_cmp';
 import type {mapFn, compareFn} from './_types';
 
-function rangeCompare<T>(x: Iterable<T>, fn: compareFn<T>=null): [T, T] {
-  var fn = fn||cmp;
-  var m: T, n: T, i = -1;
-  for(var v of x) {
-    if(++i===0) { m = n = v; }
-    if(fn(v, m)<0) m = v;
-    if(fn(v, n)>0) n = v;
-  }
-  return [m, n];
-}
-
-function rangeMap<T, U=T>(x: Iterable<T>, fn: mapFn<T, T|U>=null): [T, T] {
-  var fn = fn||id, i = -1;
-  var mk: T|U, mv: T, nk: T|U, nv: T;
-  for(var v of x) {
-    var k = fn(v, ++i, x);
-    if(i===0) { mk = nk = k; mv = nv = v; }
-    if(k<mk) { mk = k; mv = v; }
-    if(k>nk) { nk = k; nv = v; }
-  }
-  return [mv, nv];
-}
-
 /**
- * Finds smallest and largest values.
+ * Finds smallest and largest entries.
  * @param x an iterable
  * @param fc compare function (a, b)
  * @param fm map function (v, i, x)
- * @returns [min, max]
+ * @returns [smallest, largest]
  */
-function range<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): [T, T] {
+function range<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): [[number, T], [number, T]] {
   var fc = fc||cmp, fm = fm||id;
-  var mk: T|U, mv: T, nk: T|U, nv: T, i = -1;
-  for(var v of x) {
-    var k = fm(v, ++i, x);
-    if(i===0) { mk = nk = k; mv = nv = v; }
-    if(fc(k, mk)<0) { mk = k; mv = v; }
-    if(fc(k, nk)>0) { nk = k; nv = v; }
+  var mi = -1, mu: T, mv: T|U;
+  var ni = -1, nu: T, nv: T|U;
+  var i = -1;
+  for(var u of x) {
+    var v = fm(u, ++i, x);
+    if(i===0 || fc(v, mv)<0) { mi = i; mu = u; mv = v; }
+    if(i===0 || fc(v, nv)>0) { ni = i; nu = u; nv = v; }
   }
-  return [mv, nv];
+  return [[mi, mu], [ni, nu]];
 }
 export default range;
