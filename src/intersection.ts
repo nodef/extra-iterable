33aef85ebc3b27e7a4cbab0a4d8d05dafdf9a22c
Subhajit Sahu <wolfram77@gmail.com>
2020-06-15 11:44:57 +0530

    :watermelon: cool fruit ;intersection no multiple calls to fm

--- src/intersection.ts
+++ src/intersection.ts
@@ -1,17 +1,7 @@
-import many from './many';
 import id from './_id';
 import cmp from './_cmp';
 import uniques from './_uniques';
 import type {compareFn, mapFn} from './_types';
-
-function* intersectionCompare<T>(x: Iterable<T>, y: Iterable<T>, fn: compareFn<T>=null): IterableIterator<T> {
-  var fn = fn||cmp;
-  var y = many(y);
-  x: for(var u of x) {
-    for(var v of y)
-      if(fn(u, v)===0) { yield u; continue x; }
-  }
-}
 
 function* intersectionMap<T, U=T>(x: Iterable<T>, y: Iterable<T>, fn: mapFn<T, T|U>=null): IterableIterator<T> {
   var s = uniques(y, fn);
@@ -24,13 +14,11 @@ function* intersectionMap<T, U=T>(x: Iterable<T>, y: Iterable<T>, fn: mapFn<T, T
 
 function* intersectionDual<T, U=T>(x: Iterable<T>, y: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): IterableIterator<T> {
   var fc = fc||cmp, fm = fm||id;
-  var y = many(y), i = -1;
+  var y1 = [...y].map(fm), i = -1;
   x: for(var u of x) {
-    var u1 = fm(u, ++i, x), j = -1;
-    for(var v of y) {
-      var v1 = fm(v, ++j, y);
+    var u1 = fm(u, ++i, x);
+    for(var v1 of y1)
       if(fc(u1, v1)===0) { yield u; continue x; }
-    }
   }
 }
 
