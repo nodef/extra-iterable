8e215107c22930f35fee30600b34d2a284d9aed8
Subhajit Sahu <wolfram77@gmail.com>
2020-06-15 15:13:14 +0530

    :watermelon: cool fruit ;unique

--- src/isPrefix.ts
+++ src/isPrefix.ts
@@ -1,14 +1,24 @@
-import searchPrefix from './searchPrefix';
+import id from './_id';
+import cmp from './_cmp';
 import type {mapFn, compareFn} from './_types';
 
 /**
- * Checks if iterable starts with a prefix.
+ * Searches a prefix.
  * @param x an iterable
  * @param y prefix?
  * @param fc compare function (a, b)
  * @param fm map function (v, i, x)
  */
 function isPrefix<T, U=T>(x: Iterable<T>, y: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): boolean {
-  return searchPrefix(x, y, fc, fm)>=0;
+  var fc = fc||cmp, fm = fm||id;
+  var ix = x[Symbol.iterator](), i = -1;
+  for(var v of y) {
+    var {value, done} = ix.next();
+    if(done) return false;
+    var u1 = fm(value, ++i, x);
+    var v1 = fm(v, i, y);
+    if(fc(u1, v1)!==0) return false;
+  }
+  return true;
 }
 export default isPrefix;
--- src/unique.ts
+++ src/unique.ts
@@ -1,5 +1,27 @@
-import union from './union';
 import type {compareFn, mapFn} from './_types';
+import id from './_id';
+import cmp from './_cmp';
+
+function* uniqueMap<T, U=T>(x: Iterable<T>, fn: mapFn<T, T|U>=null): IterableIterator<T> {
+  var fn = fn||id;
+  var us = new Set(), i = -1;
+  for(var v of x) {
+    var v1 = fn(v, ++i, x);
+    if(us.has(v1)) continue;
+    us.add(v1); yield v;
+  }
+}
+
+function* uniqueDual<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): IterableIterator<T> {
+  var fc=fc||cmp, fm = fm||id;
+  var us = [], i = -1;
+  x: for(var v of x) {
+    var v1 = fm(v, ++i, x);
+    for(var u1 of us)
+      if(fc(u1, v1)===0) continue x;
+    us.push(v1); yield v;
+  }
+}
 
 /**
  * Removes duplicate values.
@@ -8,6 +30,7 @@ import type {compareFn, mapFn} from './_types';
  * @param fm map function (v, i, x)
  */
 function* unique<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): IterableIterator<T> {
-  yield* union([], x, fc, fm);
+  if(fc) yield* uniqueDual(x, fc, fm);
+  else yield* uniqueMap(x, fm);
 }
 export default unique;
