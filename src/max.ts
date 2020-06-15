410e97f11166089d514344631610868da94764e2
Subhajit Sahu <wolfram77@gmail.com>
2020-06-15 17:12:54 +0530

    :watermelon: cool fruit wiki exampels

--- src/max.ts
+++ src/max.ts
@@ -6,6 +6,7 @@ import type {mapFn, compareFn} from './_types';
  * @param x an iterable
  * @param fc compare function (a, b)
  * @param fm map function (v, i, x)
+ * @returns [index, value]
  */
 function max<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): [number, T] {
   return range(x, fc, fm)[1];
