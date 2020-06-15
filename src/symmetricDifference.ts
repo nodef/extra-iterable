70e01193c78f32374a7b1c630baaa461c399e67c
Subhajit Sahu <wolfram77@gmail.com>
2020-06-15 11:21:41 +0530

    :watermelon: cool fruit ;differenceDual no multiple calls to fm

--- src/symmetricDifference.ts
+++ src/symmetricDifference.ts
@@ -4,7 +4,7 @@ import cmp from './_cmp';
 import uniques from './_uniques';
 import type {compareFn, mapFn} from './_types';
 
-function* differenceMap<T, U=T>(x: Iterable<T>, y: Iterable<T>, fn: mapFn<T, T|U>=null): IterableIterator<T> {
+function* symmetricDifferenceMap<T, U=T>(x: Iterable<T>, y: Iterable<T>, fn: mapFn<T, T|U>=null): IterableIterator<T> {
   var s = uniques(y, fn);
   var fn = fn||id, i = -1;
   for(var u of x) {
@@ -15,11 +15,13 @@ function* differenceMap<T, U=T>(x: Iterable<T>, y: Iterable<T>, fn: mapFn<T, T|U
 
 function* differenceDual<T, U=T>(x: Iterable<T>, y: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): IterableIterator<T> {
   var fc = fc||cmp, fm = fm||id;
-  var y1 = [...y].map(fm), i = -1;
+  var y = many(y), i = -1;
   x: for(var u of x) {
-    var u1 = fm(u, ++i, x);
-    for(var v1 of y1)
+    var u1 = fm(u, ++i, x), j = -1;
+    for(var v of y) {
+      var v1 = fm(v, ++j, y);
       if(fc(u1, v1)===0) continue x;
+    }
     yield u;
   }
 }
@@ -33,6 +35,6 @@ function* differenceDual<T, U=T>(x: Iterable<T>, y: Iterable<T>, fc: compareFn<T
  */
 function* difference<T, U=T>(x: Iterable<T>, y: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): IterableIterator<T> {
   if(fc) yield* differenceDual(x, y, fc, fm);
-  else yield* differenceMap(x, y, fm);
+  else yield* symmetricDifferenceMap(x, y, fm);
 }
 export default difference;
