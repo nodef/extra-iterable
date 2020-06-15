81f5267cb33dc4c8c6a0e8a33b937c8af88507ea
Subhajit Sahu <wolfram77@gmail.com>
2020-06-15 17:26:52 +0530

    :watermelon: cool fruit wiki ready

--- src/from.ts
+++ src/from.ts
@@ -1,8 +1,8 @@
 /**
  * Converts iterator to iterable.
- * @param x an iterable/iterator
+ * @param x an iterator/iterable
  */
-function from<T>(x: Iterable<T>|Iterator<T>): Iterable<T> {
+function from<T>(x: Iterator<T>|Iterable<T>): Iterable<T> {
   if(typeof x[Symbol.iterator]==='function') return x as Iterable<T>;
   return {[Symbol.iterator]() { return x as Iterator<T>; }};
 }
